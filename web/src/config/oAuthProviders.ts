418ad06d69e828bac8d8787aa2337aa7a254b6ba
Zidane Wright <zidane.wright@speurgroup.com>
2021-05-03 01:19:14 -0500

    Update oauth urls

--- web/src/config/oAuthProviders.ts
+++ web/src/config/oAuthProviders.ts
@@ -4,8 +4,8 @@ import GithubIcon from "../assets/icons/github-icon.svg";
 import TwitterIcon from "../assets/icons/twitter-icon.svg";
 
 const oAuthProviders: OAuthProvider[] = [
-    {url: `${config.baseUrl}/oauth/github/redirect`, icon: GithubIcon},
-    {url: `${config.baseUrl}/oauth/twitter/redirect`, icon: TwitterIcon}
+    {url: `${config.baseUrl}/auth/login/oauth/github/start`, icon: GithubIcon},
+    {url: `${config.baseUrl}/auth/login/oauth/twitter/start`, icon: TwitterIcon}
 ];
 
 export default oAuthProviders;
